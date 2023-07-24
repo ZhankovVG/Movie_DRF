fe6c112a3662bd25e198f0e1742bab9cb8a46c09
Vitaliy <sprinter-100m@ukr.net>
2023-07-24 11:52:40 +0300

    adding pagination

--- movie/service.py
+++ movie/service.py
@@ -1,5 +1,6 @@
 from django_filters import rest_framework as filters
 from movie.models import Movie
+from rest_framework.pagination import PageNumberPagination
 
 
 def get_client_ip(request):
@@ -23,3 +24,9 @@ class MovieFilter(filters.FilterSet):
     class Meta:
         model = Movie
         fields = ['genres', 'year']
+
+
+class PaginationMovies(PageNumberPagination):
+    page_size = 2
+    page_query_param = 'page_size'
+    max_page_size = 1000
